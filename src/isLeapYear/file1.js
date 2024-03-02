07bc6f7705c381a98f48699a52bf1966f0cde4eb
Prathmesh Urkude <Prathmeshurkude20@gmail.com>
2024-03-02 23:54:39 +0530

    Requirement 1 (Leap Year Checker)

---
+++ src/isLeapYear/file1.js
@@ -0,0 +1,7 @@
+import { isLeapYear } from './dateUtil.js';
+
+let a = 2024;
+console.log(`${a} is a leap year: ${isLeapYear(a)}`);
+
+let b = 2026;
+console.log(`${b} is a leap year: ${isLeapYear(b)}`);
---
+++ src/isLeapYear/file2.js
@@ -0,0 +1,7 @@
+import * as dateUtil from './dateUtil.js';
+
+let a = 2024;
+console.log(`${a} is a leap year:`, dateUtil.isLeapYear(a)); 
+
+let b = 2026;
+console.log(`${b} is a leap year:`, dateUtil.isLeapYear(b));
